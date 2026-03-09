9f52eb6c01aa843bf643e1535e68ae151ce0133b
Ayomide Mamukuyomi <ayomidemamukuyomi5@gmail.com>
2026-03-09 14:51:59 +0100

    Added footer, animation, coding the home section

--- portfolio/src/Page.jsx
+++ portfolio/src/Page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 import { FaCode } from "react-icons/fa6";
-import { FaRegHeart,FaLightbulb  } from "react-icons/fa";
+import { FaRegHeart,FaLightbulb,FaGithub,FaLinkedin,FaRegCopyright,FaHtml5,FaCss3Alt,FaGitAlt,FaReact,        } from "react-icons/fa";
 import { VscIssueDraft } from "react-icons/vsc";
+import { IoIosMail } from "react-icons/io";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
+import { FaLocationDot, FaSquareXTwitter } from "react-icons/fa6"; 
+import { TiStarburstOutline } from "react-icons/ti";
+import { SiJavascript,SiNpm  } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { CiMail } from "react-icons/ci";
+import { VscVscode } from "react-icons/vsc";
 
 const Page = () => {
   return (
@@ -23,8 +31,34 @@ const Page = () => {
     </div>
     </div>
 
+    <section id='home'>
+    <div className='right'>
+    <h1>Hi, I'm <br/> Mamukuyomi Ayomide</h1>
+    <p>Frontend Developer</p>
+    <p>I build clean, responsive websites with simple code and user-friendly designs. Specializing in modern web technologies to create exceptional digital experiences.</p>
+    <div className='buttons'>
+        <a href='#projects'><button>View my works</button></a>
+        <a href='#contact'><button>Get in Touch</button></a>
+
+    </div>
+    </div>
+
+    <div className='left'>
+        <div>
+            <img src=''/>
+        </div>
+        <p>Available for freelance</p>
+        <div className='socials'>
+        <a href='https://github.com/mayomide1' target='_blank' className='social-icons'><FaGithub style={{fontSize:"30px"}}/></a>
+        <a href='https://www.linkedin.com/in/ayomide-mamukuyomi/' target='_blank' className='social-icons'><FaLinkedin  style={{fontSize:"30px"}}/></a>
+        <a href='https://x.com/mayomide_1' target='_blank' className='social-icons'><FaSquareXTwitter style={{fontSize:"30px"}}/></a>
+        <a href='mailto:ayomidemamukuyomi5@gmail.com' target='_blank' className='social-icons'><CiMail style={{fontSize:"30px"}}/></a>
+        </div>
+    </div>
+    </section>
+
     <section id='about' >
-    <h2 className='about-text'>{"- Get to Know Me".toUpperCase()}</h2>
+    <h2 className='about-text'>{"Get to Know Me".toUpperCase()}</h2>
     <h1>About Me</h1>
 
     <div className='about-container'>
@@ -35,22 +69,22 @@ const Page = () => {
 
     <div className='right'>
     <div className='container'>
-    <p><FaCode/></p>
+    <p><FaCode style={{fontSize:"25px"}}/></p>
     <h2>Clean Code</h2>
     <p>I write maintainable, well-documented code that scales with your project's growth.</p>
     </div>
     <div className='container'>
-    <p><FaRegHeart/></p>
+    <p><FaRegHeart style={{fontSize:"25px"}}/></p>
     <h2>User-Focused</h2>
     <p>Every design decision I make prioritizes user experience and accessibility.</p>
     </div>
     <div className='container'>
-    <p><FaLightbulb /></p>
+    <p><FaLightbulb style={{fontSize:"25px"}}/></p>
     <h2>Innovative</h2>
     <p>I stay updated with the latest technologies to bring fresh solutions to projects.</p>
     </div>
     <div className='container'>
-    <p><VscIssueDraft/></p>
+    <p><VscIssueDraft style={{fontSize:"25px"}}/></p>
     <h2>Goal-Oriented</h2>
     <p>I focus on delivering results that meet both user needs and business objectives.</p>
     </div>
@@ -59,18 +93,18 @@ const Page = () => {
     </section>
 
     <section id='skills'>
-    <h2 className='about-text'> - WHAT I WORK WITH</h2>
+    <h2 className='about-text'> WHAT I WORK WITH</h2>
     <h1>Skills & Technologies</h1>
     
     <div className='technologies'>
-        <div className='tech'>HTML5</div>
-        <div className='tech'>CSS</div>
-        <div className='tech'>JavaScript (ES6+)</div>
-        <div className='tech'>Git & Github</div>
-        <div className='tech'>React</div>
-        <div className='tech'>Tailwind CSS</div>
-        <div className='tech'>npm</div>
-        <div className='tech'>VS Code</div>
+        <div className='tech'> <FaHtml5 />HTML5</div>
+        <div className='tech'><FaCss3Alt />CSS</div>
+        <div className='tech'><SiJavascript/>JavaScript (ES6+)</div>
+        <div className='tech'> <FaGitAlt />Git & <FaGithub />Github</div>
+        <div className='tech'><FaReact />React</div>
+        <div className='tech'><RiTailwindCssFill/>Tailwind CSS</div>
+        <div className='tech'><SiNpm />npm</div>
+        <div className='tech'><VscVscode/>VS Code</div>
     </div>
 
     <div className='about-text'>Always learning and exploring new technologies</div>
@@ -78,8 +112,47 @@ const Page = () => {
     </section> 
 
     <section id='contact'>
-    <h2 className='about-text'> - GET IN TOUCH</h2>
+    <h2 className='about-text'>GET IN TOUCH</h2>
     <h1>Let's Connect</h1>
+    <div className='contact-cards'>
+        <a href='mailto:ayomidemamukuyomi5@gmail.com'>
+        <div className='contact-card'>
+        <IoIosMail style={{fontSize:"30px"}}/>
+        <div className="card-detail">
+        <p>Email</p>
+        <a href='mailto:ayomidemamukuyomi5@gmail.com'>ayomidemamkuyomi5@gmail.com</a>
+        </div>
+        </div>
+        </a>
+
+        <a href='tel:+2347016197076'>
+        <div className='contact-card'>
+        <BsFillTelephoneOutboundFill style={{fontSize:"30px"}}/>
+        <div className="card-detail">
+        <p>Phone</p>
+        <a href='tel:+2347016197076'>07016197076</a>
+        </div>
+        </div>
+        </a>
+
+        <div className='contact-card'>
+        <FaLocationDot style={{fontSize:"30px"}}/>
+        <div className="card-detail">
+        <p>Location</p>
+        <a >LAGOS, NG</a>
+        </div>
+        </div>
+
+        <div className='socials'>
+        <a href='https://github.com/mayomide1' target='_blank' className='social-icons'><FaGithub style={{fontSize:"30px"}}/></a>
+        <a href='https://www.linkedin.com/in/ayomide-mamukuyomi/' target='_blank' className='social-icons'><FaLinkedin  style={{fontSize:"30px"}}/></a>
+        <a href='https://x.com/mayomide_1' target='_blank' className='social-icons'><FaSquareXTwitter style={{fontSize:"30px"}}/></a>
+        <a href='mailto:ayomidemamukuyomi5@gmail.com' target='_blank' className='social-icons'><CiMail style={{fontSize:"30px"}}/></a>
+        </div>
+    </div>
+    <br/>
+    <h2 className='about-text form-header'>Have a specific project in mind?</h2>
+<p>I love working on unique challenges and innovative solutions. <br/>Let's discuss your vision and make it reality.</p>
     <form>
     <div className='contact-info'>
         <div className='contact-info-group'>
@@ -93,9 +166,20 @@ const Page = () => {
     </div>
     <label className='label'>Your Message</label>
     <textarea placeholder='Write your message here' rows={5} />
-    <button type='submit'>Send Message</button>
+    <button type='submit'><a href='mailto:ayomidemamukuyomi5@gmail.com'>Send Message</a></button>
     </form>
     </section>
+
+    <div className='footer'>
+        <p>MAYOMIDE</p>
+        <div className='socials'>
+        <a href='https://github.com/mayomide1' target='_blank' className='social-icons'><FaGithub style={{fontSize:"30px"}}/></a>
+        <a href='https://www.linkedin.com/in/ayomide-mamukuyomi/' target='_blank' className='social-icons'><FaLinkedin  style={{fontSize:"30px"}}/></a>
+        <a href='https://x.com/mayomide_1' target='_blank' className='social-icons'><FaSquareXTwitter style={{fontSize:"30px"}}/></a>
+        <a href='mailto:ayomidemamukuyomi5@gmail.com' target='_blank' className='social-icons'><CiMail style={{fontSize:"30px"}}/></a>
+        </div>
+        <p><FaRegCopyright /> {new Date().getFullYear()}</p>
+    </div>
     </div>
   )
 }
