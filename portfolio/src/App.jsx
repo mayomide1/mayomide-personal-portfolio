2758108d2cac029ee06303ee4e81669f2e83e139
Mamukuyomi Ayomide Emmanuel <ayomidemamukuyomi5@gmail.com>
2026-09-03 23:31:13 +0100

    Updated my portfolio with two new projects

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Page from "./Page";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <>
-    <Page/>
+      <Page />
     </>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
